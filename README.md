bfc8ceae6bdb7a3c0a702bb5f8da8b56577a1149
lidvitha <133883115+lidvitha@users.noreply.github.com>
2023-07-13 22:17:41 +0530

    Login_Signup

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- .env config -->
+
+PORT = 8080
+NODE_MODE = development
+MONGO_URL = mongodb+srv://admin:admin@cluster0.grut4dv.mongodb.net/?retryWrites=true&w=majority
+JWT_SECRET = XYZ
---
+++ client/src/Data/data.js
@@ -0,0 +1,36 @@
+export const userMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa-solid fa-list",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
+
+// admin menu
+export const adminMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Users",
+    path: "/users",
+    icon: "fa-solid fa-user",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
